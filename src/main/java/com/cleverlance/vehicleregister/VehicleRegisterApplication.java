1085dafb96e053ceaab4be6b093ea3a13de89d2b
David Dušek <david.dusek@cleverlance.com>
2019-01-14 16:11:26 +0100

    initial commit

---
+++ src/main/java/com/cleverlance/vehicleregister/VehicleRegisterApplication.java
@@ -0,0 +1,14 @@
+package com.cleverlance.vehicleregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleRegisterApplication.class, args);
+	}
+
+}
+
